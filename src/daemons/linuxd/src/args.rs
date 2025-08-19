16c90da519221577723d1ed06dac2f3e00690cf4
Carlos Segarra <carlos@carlossegarra.com>
2025-08-19 03:28:54 +0000

    [daemons] F: Support Linuxd Deployment in L2 VMs
    In this commit I extend linuxd to support deployment inside an L2 VM.
    Most importantly, when deployed inside an L2 VM linuxd will always be
    restored from a snapshot. As a consequence, we enable a 'trap' mechanism
    such that liuxd can safely wait at a well-known location before the
    snapshot takes place.
    
    Signed-off-by: Carlos Segarra <carlos@carlossegarra.com>

--- src/daemons/linuxd/src/args.rs
+++ src/daemons/linuxd/src/args.rs
@@ -31,6 +31,8 @@ pub struct Args {
     gateway_bind_sockaddr_type: Option<String>,
     /// Log to file?
     log_to_file: bool,
+    /// Deployed in an L2 VM?
+    l2: bool,
 }
 
 //==================================================================================================
@@ -54,6 +56,8 @@ impl Args {
     const OPT_GATEWAY_BIND_SOCKET_TYPE: &'static str = "-gateway-bind-socket-type";
     /// Command-line option for log redirecting.
     const OPT_LOGFILE: &'static str = "-log-to-file";
+    /// Command-line option for signaling deployment in an L2 VM.
+    const OPT_L2: &'static str = "-l2";
 
     ///
     /// # Description
@@ -77,6 +81,7 @@ impl Args {
         let mut gateway_bind_sockaddr: Option<String> = None;
         let mut gateway_bind_sockaddr_type: Option<String> = None;
         let mut log_to_file: bool = false;
+        let mut l2: bool = false;
 
         let mut i: usize = 1;
         while i < args.len() {
@@ -112,6 +117,9 @@ impl Args {
                 Self::OPT_LOGFILE => {
                     log_to_file = true;
                 },
+                Self::OPT_L2 => {
+                    l2 = true;
+                },
                 invalid_arg => {
                     return Err(anyhow::anyhow!("invalid argument: {invalid_arg}"));
                 },
@@ -138,6 +146,7 @@ impl Args {
             gateway_bind_sockaddr,
             gateway_bind_sockaddr_type,
             log_to_file,
+            l2,
         })
     }
 
@@ -254,5 +263,18 @@ impl Args {
     ///
     pub fn log_to_file(&self) -> bool {
         self.log_to_file
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Returns whether we are deployed inside an L2 VM.
+    ///
+    /// # Returns
+    ///
+    /// If deployed inside an L2 VM.
+    ///
+    pub fn l2(&self) -> bool {
+        self.l2
     }
 }
--- src/daemons/linuxd/src/main.rs
+++ src/daemons/linuxd/src/main.rs
@@ -63,7 +63,6 @@ use ::sys::{
 use ::syscomm::{
     Socket,
     SocketListener,
-    SocketStream,
     SocketType,
 };
 use sys::pm::ThreadIdentifier;
@@ -94,6 +93,9 @@ pub fn main() -> Result<()> {
     let control_plane_sockaddr: String = args.control_plane_sockaddr();
     let user_vm_sockaddr: String = args.user_vm_bind_sockaddr();
     let gateway_sockaddr: Option<String> = args.gateway_bind_sockaddr();
+
+    // Deployed in an L2 VM?
+    let in_l2: bool = args.l2();
 
     // Work-out the socket-types.
     let control_plane_socket_type: SocketType = match args.control_plane_socket_type() {
@@ -129,22 +131,6 @@ pub fn main() -> Result<()> {
         None => DEFAULT_GATEWAY_SOCKET_TYPE,
     };
 
-    // Connect the control-plane socket.
-    let control_plane_stream: SocketStream =
-        match SocketStream::connect(control_plane_socket_type, control_plane_sockaddr.clone()) {
-            Ok(socket) => {
-                info!("Connected to control plane on: {:?}", control_plane_sockaddr);
-                socket
-            },
-            Err(e) => {
-                error!(
-                    "failed to connect to control-plane socket address (address={}, error={e:?})",
-                    control_plane_sockaddr.clone()
-                );
-                anyhow::bail!("failed to connect to control-plane socket address");
-            },
-        };
-
     // Start listening for incoming connections from user VMs associated to this linuxd instance.
     let user_vm_listener: SocketListener =
         match Socket::bind(user_vm_bind_socket_type, user_vm_sockaddr.clone()) {
@@ -174,11 +160,16 @@ pub fn main() -> Result<()> {
         None => None,
     };
 
-    let procd: LinuxDaemon =
-        match LinuxDaemon::init(control_plane_stream, user_vm_listener, gateway_listener) {
-            Ok(procd) => procd,
-            Err(e) => panic!("failed to initialize process manager daemon (error={e:?})"),
-        };
+    let procd: LinuxDaemon = match LinuxDaemon::init(
+        control_plane_sockaddr,
+        control_plane_socket_type,
+        user_vm_listener,
+        gateway_listener,
+        in_l2,
+    ) {
+        Ok(procd) => procd,
+        Err(e) => panic!("failed to initialize process manager daemon (error={e:?})"),
+    };
 
     // Run main procd loop.
     let procd_ret = procd.run();
