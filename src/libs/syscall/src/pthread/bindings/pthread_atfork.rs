db3dc26e3e14d491c4b846d8c41158d4531c1825
danbugs <danilochiarlone@gmail.com>
2025-09-11 23:34:35 +0000

    added pthread_atfork
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

---
+++ src/libs/syscall/src/pthread/bindings/pthread_atfork.rs
@@ -0,0 +1,25 @@
+// Copyright(c) The Maintainers of Nanvix.
+// Licensed under the MIT License.
+
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use ::sysapi::ffi::c_int;
+
+//==================================================================================================
+// Standalone Functions
+//==================================================================================================
+
+// TODO: add description
+#[allow(clippy::missing_safety_doc)]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn pthread_atfork(
+    _prepare: Option<extern "C" fn()>,
+    _parent: Option<extern "C" fn()>,
+    _child: Option<extern "C" fn()>,
+) -> c_int {
+    // TODO: https://github.com/nanvix/nanvix/issues/483
+    ::syslog::warn!("pthread_atfork(): not implemented");
+    0
+}
